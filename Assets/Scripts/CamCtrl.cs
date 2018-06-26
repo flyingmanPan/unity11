43278388b6f20feb6005a886a7e0f06cacb08bd1
ZZFM <flyingman_pan@outlook.com>
2018-06-26 23:27:43 +0800

    init

---
+++ Assets/Scripts/CamCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamCtrl: MonoBehaviour
+{
+    public Transform target;
+    public float distanceH = 20f;
+    public float distanceV = 7f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 nextpos = target.forward * -1 * distanceH + target.up * distanceV + target.position;
+        this.transform.position = nextpos;
+        this.transform.LookAt(target);
+    }
+}
